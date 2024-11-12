f689f8e9f632e4b084a2118f2cbb4db969bcfb2a
Anton PP <anton-la@mail.ru>
2024-11-13 00:21:42 +0300

    Add write erd file

--- ddl-tocsv/main.py
+++ ddl-tocsv/main.py
@@ -4,6 +4,60 @@ from simple_ddl_parser import DDLParser
 
 SOURCE_DIR = '../ddl-source/'
 TARGET_DIR = '../csv-target/'
+ERD_START = '''
+@startuml
+
+!theme plain
+hide empty methods
+
+!procedure $schema($name, $slug)
+package "$name" as $slug <<Rectangle>>
+!endprocedure
+
+!procedure $table($name, $slug)
+entity "<b>$name</b>" as $slug << (T, Orange) table >>
+!endprocedure
+
+!procedure $view($name, $slug)
+entity "<b>$name</b>" as $slug << (V, Aquamarine) view >>
+!endprocedure
+
+!procedure $pk($name)
+<color:#GoldenRod><&key></color> <b>$name</b>
+!endprocedure
+
+!procedure $fk($name)
+<color:#Silver><&key></color> $name
+!endprocedure
+
+!procedure $pfk($name)
+<color:#GoldenRod><&key></color><color:#Silver><&key></color> <b>$name</b>
+!endprocedure
+
+!procedure $column($name)
+{field} <color:#White><&media-record></color> $name
+!endprocedure
+'''
+ERD_END = '''
+@enduml
+'''
+ERD_SCHEMA = '''
+$schema("{name}", "{alias}") '''
+ERD_SCHEMA_END = '''
+}
+'''
+ERD_TABLE = '''
+    $table("{name}", "{alias}") '''
+ERD_TABLE_END = '''
+    }
+'''
+ERD_COLUMN = '''
+        ${type}("{name}"): {data_type} {constrains}'''
+ERD_RELATION_FROM = '''
+{source_schema}.{source_table}::{source_column}'''
+ERD_RELATION_TO = '''{target_schema}.{target_table}::{target_column} : {fk_title}
+'''
+ERD_RELATION_LINE = ' ||--o{ '
 
 
 # Получение констрейнов для колонки
@@ -90,12 +144,25 @@ def write_csv(sql_statements):
 
 
 def write_erd(sql_statements):
-    for statement in sql_statements:
+    erd = ERD_START
+    # for statement in sql_statements:
+    erd += ERD_SCHEMA.format(name='sch_name', alias='sch_name') + '{\n'
+    erd += ERD_TABLE.format(name='t_name', alias='t_name') + '{'
+    erd += ERD_COLUMN.format(type='pk', name='id', data_type='text', constrains='')
+    erd += ERD_COLUMN.format(type='pfk', name='name', data_type='text', constrains='NOT NULL')
+    erd += ERD_COLUMN.format(type='column', name='created', data_type='timestampz', constrains='DEFAULT now()')
+    erd += ERD_TABLE_END
+    erd += ERD_SCHEMA_END
+    erd += ERD_RELATION_FROM.format(source_schema='sch_name', source_table='t_name', source_column='name')
+    erd += ERD_RELATION_LINE
+    erd += ERD_RELATION_TO.format(target_schema='sch_name', target_table='t_name', target_column='pk', fk_title='fk_test')
+    erd += ERD_END
+    with Path('{}erd.puml'.format(TARGET_DIR)).open('w', encoding='utf-8') as outfile:
+        outfile.write(erd)
 
-        return False
 
-
-# Находим все sql файлы в SOURCE_DIR
+# Создаем TARGET_DIR и находим все sql файлы в SOURCE_DIR
+Path(TARGET_DIR).mkdir(parents=True, exist_ok=True)
 list_of_files = [f for f in Path().glob(SOURCE_DIR + "*.sql")]
 if len(list_of_files) == 0:
     print('No sql files in directory: {}'.format(SOURCE_DIR))
@@ -104,4 +171,5 @@ statements = get_all_statements(list_of_files)
 if not statements:
     print('No sql statement in directory: {}'.format(SOURCE_DIR))
     exit()
-write_csv(statements)
+# write_csv(statements)
+write_erd(statements)
